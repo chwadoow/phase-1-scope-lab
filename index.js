eb0dbec1207324e95d9fdeb3710166fbc5296e96
moses ngige <moses32ngigz@gmail.com>
2022-06-09 10:35:46 +0300

    Done.

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ var customerName = 'bob';
 
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
-  //return customerName
+ 
 }
-//console.log (customerName);
-//console.log (upperCaseCustomerName());
+
 
 function setBestCustomer() {
   bestCustomer = 'not bob';
 }
-//console.log(bestCustomer); 
+ 
 
 function overwriteBestCustomer() {
   bestCustomer = 'maybe bob';
   }
-//console.log(overwriteBestCustomer()); error
+
 const leastFavoriteCustomer='poor marley';
 
 function changeLeastFavoriteCustomer() {
-    leastFavoriteCustomer = 'rich marley';}
-//console.log(leastFavoriteCustomer);
-//console.log(leastFavoriteCustomer());
+    leastFavoriteCustomer = 'rich marley'
+    ;}
