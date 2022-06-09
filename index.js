548f6548947a3897d0aa9064113f5758f08e903b
moses ngige <moses32ngigz@gmail.com>
2022-06-09 10:32:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  //return customerName
+}
+//console.log (customerName);
+//console.log (upperCaseCustomerName());
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+//console.log(bestCustomer); 
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+  }
+//console.log(overwriteBestCustomer()); error
+const leastFavoriteCustomer='poor marley';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'rich marley';}
+//console.log(leastFavoriteCustomer);
+//console.log(leastFavoriteCustomer());
